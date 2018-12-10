a41d116e265724dad426ddf1e36bfceb107f0093
Philipp Vitkovsky <philipp.vitkovsky@gmail.com>
2018-12-10 22:15:35 +0200

    toString in timed DTO

--- src/main/com/appointments/dto/TimedAppointmentDTO.java
+++ src/main/com/appointments/dto/TimedAppointmentDTO.java
@@ -2,9 +2,10 @@ package com.appointments.dto;
 
 import java.util.UUID;
 
-import com.appointments.util.date.range.IDateRange;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.appointments.util.date.range.IDateRange;
 
 /**
  * dto for appointment creation; appointment is event with Organizer and Attendee; 
@@ -31,7 +32,7 @@ public abstract class TimedAppointmentDTO extends BaseAppointmentDTO implements 
 	@Override
 	public String toString() {
 		return "TimedAppointmentDTO [requestType=" + getRequestType() + ", Time " + getDateRange().toString() + ", requestId="
-				+ getRequestId() + ", eventId=" + getEventId() + ", organizer=" + getOrganizer() + ", attendee=" + getAttendee()
+				+ getRequestId() + ", timestamp= " + getTimestamp() + ", eventId=" + getEventId() + ", organizer=" + getOrganizer() + ", attendee=" + getAttendee()
 				+ ", registered=" + isRegistered() + ", responded=" + isResponded() + ", complete=" + isComplete() + "]";
 	}
 
